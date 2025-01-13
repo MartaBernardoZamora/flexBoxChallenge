115b19b97a3b6e4b7a1d1917ca4e71ea61a4afb0
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-13 18:15:48 +0100

    handleClick with hidden and doble DIV save

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*VERSIÓN CON HIDDEN y currentTarget encapsulando el div (para cuando tocas en otro elemento y quieres el de debajo)
 const handleCardClick = (e) => {
     let styleClickedCard = e.currentTarget.firstElementChild.style;
     styleClickedCard.visibility = styleClickedCard.visibility === "hidden" ? "initial": "hidden";
@@ -7,5 +8,18 @@ const setCard = () => {
     cards.forEach((card) =>{
         card.onclick=(e)=>handleCardClick(e);
     })
+}*/
+const handleCardClick = (e) => {
+    let styleClickedCard = e.target.style;
+    styleClickedCard.opacity = styleClickedCard.opacity === "0" ? "1" : "0";
+};
+const setCard = () => {
+    const cards=document.querySelectorAll('.card');
+    cards.forEach((card) =>{
+        card.onclick=(e)=>handleCardClick(e);
+    })
 }
-window.onload = () => setCard();
+window.onload = () => {
+    /*setCard()*/;
+    setCard();
+};
