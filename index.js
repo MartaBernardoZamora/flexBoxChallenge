63568e2e27fffc6748dcbe94d27335e1d13893b4
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-13 22:08:21 +0100

    fix to upload to main

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const handleCardClick = (e) => {
-    let styleClickedCard = e.target.style;
+    let styleClickedCard = e.currentTarget.style;
     styleClickedCard.opacity = styleClickedCard.opacity === "0" ? "1" : "0";
 };
 const setCard = () => {
-    const cards=document.querySelectorAll('.card');
+    const cards=document.querySelectorAll('.container__card');
     cards.forEach((card) =>{
         card.onclick=(e)=>handleCardClick(e);
     })
