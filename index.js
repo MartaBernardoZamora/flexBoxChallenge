b3c79b60c5134853339b0aef517759ece36e2c43
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-13 18:16:42 +0100

    js_file complete funcionality with opacity

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*VERSIÓN CON HIDDEN y currentTarget encapsulando el div (para cuando tocas en otro elemento y quieres el de debajo)
-const handleCardClick = (e) => {
-    let styleClickedCard = e.currentTarget.firstElementChild.style;
-    styleClickedCard.visibility = styleClickedCard.visibility === "hidden" ? "initial": "hidden";
-};
-const setCard = () => {
-    const cards=document.querySelectorAll('.card');
-    cards.forEach((card) =>{
-        card.onclick=(e)=>handleCardClick(e);
-    })
-}*/
 const handleCardClick = (e) => {
     let styleClickedCard = e.target.style;
     styleClickedCard.opacity = styleClickedCard.opacity === "0" ? "1" : "0";
@@ -20,6 +9,5 @@ const setCard = () => {
     })
 }
 window.onload = () => {
-    /*setCard()*/;
     setCard();
 };
