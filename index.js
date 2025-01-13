0efd102ff8969c61a95d768f6ad9096aa164fd33
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-13 21:31:14 +0100

    change_to_grid

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const handleCardClick = (e) => {
     styleClickedCard.opacity = styleClickedCard.opacity === "0" ? "1" : "0";
 };
 const setCard = () => {
-    const cards=document.querySelectorAll('.card');
+    const cards=document.querySelectorAll('.container__card');
     cards.forEach((card) =>{
         card.onclick=(e)=>handleCardClick(e);
     })
 }
+const setDisplay =()=>{
+    
+}
 window.onload = () => {
     setCard();
+    document.getElementById('displayButton').onclick=()=>setDisplay();
 };
