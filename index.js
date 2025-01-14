abcdc381fd93c79de8ad745b52fb24325ee00af2
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-14 10:07:49 +0100

    Flex/Grid toggle button

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const setCard = () => {
     })
 }
 const setDisplay =()=>{
-    
+    const cards=document.querySelectorAll('.container__card');
+    cards.forEach((card) =>{
+        card.classList.contains("container_card--flex") && card.classList.remove("container_card--flex");
+    })
+    const container=document.querySelector('.container');
+    container.classList.toggle("container--flex");
+    container.classList.toggle("container--grid");
+    const isFlex = document.querySelector('.buttonContainer__p').textContent.includes("Flexbox")
+    document.querySelector('.buttonContainer__p').textContent = `Actualmente, esta web utiliza ${isFlex ? "Grid" : "Flexbox"}`;
+    document.querySelector('.buttonContainer__button').textContent = `Cambiar a ${isFlex ? "Flexbox" : "Grid"}`;
 }
 window.onload = () => {
     setCard();
